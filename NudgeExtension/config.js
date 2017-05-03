f9cd7369bf608357caddb06d9bfb9099832da9b1
Kristo Mikkonen <mikkonek@gmail.com>
2017-05-03 18:34:56 +0100

    Adding api call for seeing user browsing objets to server.

--- NudgeExtension/config.js
+++ NudgeExtension/config.js
@@ -1,7 +1,9 @@
 var config = {
-  'debug': true
+  'debug': true,
+  'apiEndpoint': 'http://ec2-34-252-87-55.eu-west-1.compute.amazonaws.com:3000/'
 }
 
+// http://ec2-34-252-87-55.eu-west-1.compute.amazonaws.com:3000/
 
 //  global debug option for logger
 debugMode = config.debug;
