8b8119cdd3f638d2fea96500a1dfac58e7578922
lgwb89 <nudgewareio@gmail.com>
2017-05-11 12:11:16 +0200

    Finalise debugger UI

--- NudgeExtension/resources/js/debugger.js
+++ NudgeExtension/resources/js/debugger.js
@@ -1,33 +1,29 @@
 // Copyright 2016, Nudge, All rights reserved.
 
-
 $(document).ready(function() {
   listener(debuggerInit());
 });
 
-// image caching because svg arrives too late!
-
 function debuggerInit() {
   var nudge_db = createEl(document.body, 'div', 'nudge_db');
     nudge_db.innerHTML =
-      '<div id="db_container">' +
-        '<div id="db_message">' +
-          '<div id="db_message_contents">' +
+        '<div id="db_container">' +
+          '<div id="db_message">' +
+            '<div id="db_message_contents">' +
+              'Loading...' +
+            '</div>' +
           '</div>' +
-        '</div>' +
-      '</div>';    
+      '</div>';
   var messageContents = document.getElementById('db_message_contents');
   return messageContents;
 }
-
-console.log(messageContents);
 
 function listener(element) {
   chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
     if (request.type === "debug_updater") {
       if (element) {
-        element.innerHTML = logMinutes(request.startTime/1000) + ' before, ' + 'now ' + logMinutes(request.currentTime) + '. ' + request.visits + ' visits.';
+        element.innerHTML = request.domain + ' | Now: ' + logMinutes(request.secondsIn) + ' | Total: ' + logMinutes(request.total) + ' | Before: ' + logMinutes(request.before/1000) + ' | Visits: ' + request.visits + ' | Time per visit: ' + logMinutes((request.total)/request.visits);
       }
     }
   });
