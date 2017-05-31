c7db250dc1375613054a5bc0fb2a4d0b4c9abf09
lgwb89 <nudgewareio@gmail.com>
2017-05-31 02:00:17 +0100

    Option for offSwitch + insert tabIdler

--- NudgeExtension/resources/js/controller.js
+++ NudgeExtension/resources/js/controller.js
@@ -1,13 +1,22 @@
 // Copyright 2016, Nudge, All rights reserved.
 
+var offSwitch = true;
+
 chrome.storage.sync.get(null, function(items) {
   if (domainChecker(window.location.href, items.domains_setting)) {
-    console.log("go");
-    init();
+    if (offSwitch) {
+	  init();	
+    }
+    tabIdler();
   }
 });
 
 function init() {
   chrome.runtime.sendMessage({ type: "inject_switch"}, function(response) {
+  });
+}
+
+function tabIdler() {
+  chrome.runtime.sendMessage({ type: "inject_tabidler"}, function(response) {
   });
 }
