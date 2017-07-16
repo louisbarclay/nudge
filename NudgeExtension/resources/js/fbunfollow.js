835ffa44fdf6214dfacee4d84f3655efc7f8e8e3
lgwb89 <nudgewareio@gmail.com>
2017-07-16 23:24:11 +0200

    fb unfollow tweaks

--- NudgeExtension/resources/js/fbunfollow.js
+++ NudgeExtension/resources/js/fbunfollow.js
@@ -8,19 +8,18 @@ var user_id = '';
 // Store profile data here (friends and pages)
 var profileData = [];
 
-friendAndPageToggler(selectRandomFromArray(profileData));
+// when running for first time, store time in chrome storage
+// every time you go back to fb after, keep running if enough time has elapsed
+// randomise number of profiles to unfollow every day. max 100
 
 console.log(selectRandomFromArray(profileData));
 
 $(document).ready(function() {
   // Get the fb_dtsg token that must be passed to get a successful response to an XMLHttpRequest from Facebook
   try {
+    // Get fb_dtsg
     fb_dtsg = document.getElementsByName("fb_dtsg")[0].value;
-    console.log(fb_dtsg);
-    // var el = document.createElement("div");
-    //   el.innerText = el.textContent = fb_dtsg;
-    //   fb_dtsg = el.innerHTML; 
-    // Get user_id (1 method)
+    // Get user_id
     if (document.cookie.match(/c_user=(\d+)/)) {
       if (document.cookie.match(/c_user=(\d+)/)[1]) {
         user_id = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
@@ -28,11 +27,7 @@ $(document).ready(function() {
         console.log(user_id);
       }
     }
-    if (!document.getElementById('uf_message_contents')) {
-      bar();  
-    }
-    barChangeText('Getting friend and page list...');
-    friendAndPageListGenerator(i, unfollowList);
+    friendAndPageListGenerator(unfollow);
   } catch (error) {
     // Error catching
   }
@@ -57,6 +52,36 @@ function barChangeText(newText) {
   messageContents.innerHTML = newText;    
 }
 
+var barState = {
+  initial: 'auto',
+
+};
+
+var clickNumber = 0;
+
+// Click handler
+function clickHandler() {
+  var bar_container = document.getElementById('uf_message_contents');
+  bar_container.onclick = function() {
+    clickNumber++;
+    if (clickNumber === 1) {
+      barChangeText("Click to confirm that you want to unfollow all your friends");
+      clickHandler();
+      return;
+    }
+    if (clickNumber === 2) {
+      friendAndPageToggler(unfollow);
+      return;
+    }
+  };
+}
+
+$(document).keyup(function(key) {
+  var bar_container = document.getElementById('uf_message_contents');
+  if (key.keyCode === 27) {
+    deleteEl(bar_container);
+  }
+});
 
 // Keyboard shortcuts for testing
 if (window.addEventListener) {
@@ -64,30 +89,13 @@ if (window.addEventListener) {
   window.addEventListener("keydown", function(e) {
           letters.push(e.key);
           if ([letters.slice(-2)[0],letters.slice(-1)[0]].toString() === prompt1.toString()) {
-            friendAndPageToggler(profileData, selectRandomFromArray(profileData));
           }
   }, true);
 }
 
-// TODO: test first. try unfollowing 1 profile then refollowing. If it works, offer the feature
-
-// URLs to do XMLHttpRequests to:
-
-// unfollow show profile url
-
-var unfollowList = "https://www.facebook.com/feed_preferences/profile_list_more/?card_type=unfollow&filter=all&page=";
-var refollowList = 'https://www.facebook.com/feed_preferences/profile_list_more/?card_type=refollow&filter=all&page=';
-
-// unfollow action url
-// https://www.facebook.com/ajax/follow/unfollow_profile.php?dpr=1
-// refollow action url
-// https://www.facebook.com/ajax/follow/follow_profile.php?dpr=1
-
 // Create a random delay between XMLHttpRequests
-function randomTime() {
+function randomTime(floor, variance) {
   var ms = 1000;
-  var floor = 0.1;
-  var variance = 0.1;
   return Math.floor(ms * (floor + (Math.random() * variance)));
 }
 
@@ -96,63 +104,105 @@ var i = 0;
 var keepGoing = true;
 
 
-// Not in use any more - loop function with in-built delay
-// function loopIt() {
-//   setTimeout(function () {
-//   friendAndPageListGenerator(i);
-//     i++;
-//     console.log(i, keepGoing);
-//     if (i < 4 && keepGoing) {
-//         loopIt();
-//     }
-//   }, randomTime());
-// }
-
-
 function selectRandomFromArray(array) {
   var randomNumber = Math.floor(Math.random() * array.length);
   return randomNumber;
 }
 
+// tiny use case: only show the thing if the person is fololwing any friends
+
+var unfollow = {
+  listUrl: "https://www.facebook.com/feed_preferences/profile_list_more/?card_type=unfollow&filter=all&page=",
+  actionUrl: "https://www.facebook.com/ajax/follow/unfollow_profile.php",
+  profiles: [],
+  messages: {
+    loaded: 'Ready to unfollow ',
+    empty: 'No profiles to unfollow',
+    start: 'Trying to unfollow ',
+    success: 'Successfully unfollowed ', // FIXME: Recommend that you go and do something else - leave tab open
+    fail: "Couldn't unfollow "
+  },
+  i: 0,
+  j: 0,
+  safetylock: 2000, // FIXME
+  keepGoing: true,
+  verifText: {
+    start: 'Arbiter.inform("UnfollowUser", {"profile_id":',
+    end: '});'
+  }
+};
+
+var refollow = {
+  listUrl: 'https://www.facebook.com/feed_preferences/profile_list_more/?card_type=refollow&filter=all&page=',
+  actionUrl: "https://www.facebook.com/ajax/follow/follow_profile.php?dpr=1",
+  profiles: [],
+  messages: {
+    loaded: 'Ready to refollow ',
+    empty: 'No profiles to refollow',
+    start: 'Trying to refollow ',
+    success: 'Successfully refollowed ',
+    fail: "Couldn't refollow "
+  },
+  i: 0,
+  j: 0,
+  safetylock: 2000, // that's not for toggle vs. listgen-  it's for both FIXME
+  keepGoing: true,
+  verifText: {
+    start: 'Arbiter.inform("FollowUser", {"profile_id":',
+    end: '});'
+  }
+};
+
 // Get friend and page IDs
-function friendAndPageListGenerator(i, whichList) {
-  console.log(i);
+function friendAndPageListGenerator(option) {
+  // Empty out profile storage if you're on the first loop
+  if (option.i === 0) {
+    option.keepGoing = true;
+  }
   // Define params variable for passing with the XMLHttpRequest send
   var params = '';
   // Create new XMLHttpRequest
   friendandpagelist_get = new XMLHttpRequest();
   // Set the url etc. - note that 'i' will change as we load more pages worth of data
-  friendandpagelist_get.open("POST", whichList + i + "&dpr=1", true);
+  friendandpagelist_get.open("POST", option.listUrl + option.i + "&dpr=1", true);
   // Tweak to get headers in right format
   friendandpagelist_get.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   // Listener for when it has worked
   friendandpagelist_get.onreadystatechange = function() {
     if (friendandpagelist_get.readyState == 4) {
       // Get data in the right format
-      console.log(friendandpagelist_get);
       var data = friendandpagelist_get.responseText;
       data = data.substr(data.indexOf('{'));
       data = JSON.parse(data);
+      var hasMoreData = false;
+      if (typeof data.payload.hasMoreData != 'undefined') {
+        hasMoreData = data.payload.hasMoreData;
+      }
       data = data.payload.profiles;
-      console.log(data);
+      if (data.length > 0) {
+        console.log('Loaded ' + data[0].name + ' and ' + (data.length - 1) + ' other profiles');
+      }
       // Set keepGoing to false if you reach a stage where you get less than 24 
       for (var j = 0; j < data.length; j++) {
-        profileData.push(data[j]);
-        if (j === data.length - 1) {
-          console.log(profileData);
+        option.profiles.push(data[j]); // Should you check you aren't adding duplicates?
+        // console.log((option.i * 24) + j + ' ' + data[j].name); // Why not 
+      }
+      if (!hasMoreData) {
+        option.i = 0;
+        option.keepGoing = false;
+        if (!document.getElementById('uf_message_contents') && option.profiles.length > 0) {
+          bar();
+          clickHandler();
+          barChangeText(option.messages.loaded + option.profiles.length + " friends (click to start, press 'Esc' to hide)");
         }
       }
-      if (data.length < 24) { // This is weak
-        keepGoing = false;
-        console.log(profileData[selectRandomFromArray(profileData)]);
-        barChangeText("Friend and page list loaded");
-      }
       // Loop again with higher 'i', unless keepGoing is false
-      if (keepGoing) {
-        i++;
-        setTimeout(function () {
-          friendAndPageListGenerator(i);  
-        }, randomTime());
+      if (option.keepGoing && option.i < option.safetylock) {
+        option.i++;
+        friendAndPageListGenerator(option);
+      } else {
+        option.keepGoing = false;
+        option.i = 0;
       }
     }
   };
@@ -167,119 +217,85 @@ function friendAndPageListGenerator(i, whichList) {
 
 // add up from unfollow and refollow . see if matches that figure you got earlier. if that makes sense.
 
-function friendAndPageToggler(profiledataarray, index) {
-  console.log(profiledataarray);
-  console.log(index);  
-  var profile_id_obj = profiledataarray[index];
-  if (typeof profile_id_obj === 'undefined') {
-    console.log('couldnt find profile');
+function friendAndPageToggler(option) {
+  // Check if there are profiles to toggle
+  console.log(option);
+  if (option.profiles.length === 0) {
+    console.log(option.messages.empty);
+    barChangeText(option.messages.empty);
     return;
   }
-  else {
+  var profile_id_obj = option.profiles[option.j];
+  if (typeof profile_id_obj === 'undefined') {
+    console.log("Couldn't find profile");
+    return;
+  } else {
+    var name = profile_id_obj.name;
     var profile_id = profile_id_obj.id;
   }
-  var startMessage = "Trying to unfollow " + profile_id_obj.name;
-  console.log(startMessage);
-  barChangeText(startMessage);
+  console.log(option.messages.start + name);
   friendandpage_toggle = new XMLHttpRequest();
-  friendandpage_toggle.open("POST", "https://www.facebook.com/ajax/follow/unfollow_profile.php?dpr=1", true);
+  friendandpage_toggle.open("POST", option.actionUrl, true);
   friendandpage_toggle.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   var params = '';
   // Parameters to make this work!
-  params += "&__a=1"; // done
-  params += "&__be=1"; // done 
-  params += "&location=100"; // done 
-  params += "&jazoest=265816973751091105452971171165865817088851118311068897554"; // done
-  params += "&__spin_r=3122250"; // done
-  params += "&__spin_b=trunk"; // done  
-  params += "&__spin_t=1498669143"; // done    
-  params += "&__rev=3122250"; // done
-  params += "&__pc=PHASED:DEFAULT"; // done    
-  params += "&__rev=3122994"; // done      
-  params += "&__af=iw"; // done  
-  // params+="&__req=12"; // don't need it
+  params += "&__a=1"; // Need this
+  params += "&location=1"; // Need this
   params += "&fb_dtsg=" + fb_dtsg;
   params += "&__user=" + user_id;
   params += "&profile_id=" + profile_id;
+  params += "&nctr[_mod]=pagelet_timeline_profile_actions";
+  params += "&__req=65";  
+  if (option === unfollow) {
+    console.log("yea");
+  }
+  // params += "&confirmed=" + 1; // add this back in only when it's needed?
   friendandpage_toggle.onreadystatechange = function() {
     if (friendandpage_toggle.readyState == 4) {
       var data = friendandpage_toggle.responseText;
       data = data.substr(data.indexOf('{'));
       data = JSON.parse(data);
-      if (typeof data != 'undefined' && data.onload[0] === ('Arbiter.inform("UnfollowUser", {"profile_id":' + profile_id + '});')) {
-        var successMessage = "Unfollowed " + profile_id_obj.name;
-        console.log(successMessage);
-        barChangeText(successMessage);
-        profileData.splice(index, 1); // error handling here?
-      } else {
-        var failMessage = "Couldn't unfollow " + profile_id_obj.name;
-        console.log(failMessage);
-        barChangeText(failMessage);
-        console.log(friendandpage_toggle);
-        // need to remove that friend from the list of people to unfollow. or reload all that data.
-      }
+      if (typeof data != 'undefined') {
+        if (typeof data.error != 'undefined') {
+          console.log(data.error);
+          console.log(data.errorDescription);
+          console.log(data.errorSummary);          
+          console.log(option.j + ' completed out of ' + option.profiles.length + ' when error hit. Please try again tomorrow');
+          return;
+        }
+        if (typeof data.onload != 'undefined' && data.onload[0] === (option.verifText.start + profile_id + option.verifText.end)) {
+          console.log(option.messages.success + name + ' (' + option.j + ' out of ' + option.profiles.length + ')');
+          barChangeText(option.messages.success + name);
+          option.j++;
+          if (option.j < option.profiles.length && option.j < option.safetylock) {
+            setTimeout(function () {
+              friendAndPageToggler(option);
+            }, randomTime(1,0.5)); // This is too aggressive - have to do every 1000ms
+          }
+        } else {
+          console.log(option.messages.fail + name);
+          barChangeText(option.messages.fail + name);
+          console.log(friendandpage_toggle);
+          // need to remove that friend from the list of people to unfollow. or reload all that data.
+        }
+      }  
     }
   };
   friendandpage_toggle.send(params);
 }
 
-/*
-profile_id:100000168534450
-location:100
-__user:1613880018
-__a:1
-__dyn:7AgNeUiFoF1t2u6aZGeFxqeCwKyaF3oyeqhf8267UKezob4q2i5U4e1FDxtu9wSwADK7HzErwHwTz9VoboGq2i58nUOfz8nxm1DADBwBx62q3O69LJ1aih1G7WwzwxUqU4Cu7oeoqAxxwICK68pUCbxyGzkfqwKxqm9Kl1qUKaxSiaDyES2Wq
-__af:iw
-__req:x
-__be:1
-__pc:PHASED:DEFAULT
-__rev:3122250
-fb_dtsg:AQEIKmn64aut:AQFXUoSnDYK6
-jazoest:265816973751091105452971171165865817088851118311068897554
-__spin_r:3122250
-__spin_b:trunk
-__spin_t:1498669143
+// took 1hr for 1400. more or less. one every 1.5 seconds, actually a bit more
 
-profile_id:500088465
-location:100
-__user:1613880018
-__a:1
-__dyn:7AgNeUiFoF1t2u6aZGeFxqeCwKyaF3oyeqhf8267UKezob4q2i5U4e1FDxtu9wSwADK7HzErwHwTz9VoboGq2i58nUOfz8nxm1DADBwBx62q3O69LJ1aih1G7WwzwxUqU4Cu7oeoqAxxwICK68pUCbxyGzkfqwKxqm9Kl1qUKaxSiaDyES2Wq
-__af:iw
-__req:y
-__be:1
-__pc:PHASED:DEFAULT
-__rev:3122250
-fb_dtsg:AQEIKmn64aut:AQFXUoSnDYK6
-jazoest:265816973751091105452971171165865817088851118311068897554
-__spin_r:3122250
-__spin_b:trunk
-__spin_t:1498669143
-*/
+// FIXME: Only you will know. IMPORTANT MESSAGE TO INCLUDE
 
+var errorUrl = "https://www.facebook.com/ajax/haste-response/?modules=ConfirmationDialog&__user=1613880018&__a=1";
 
-/*
-__a:1
-__be:1
-__pc:PHASED:DEFAULT
-__rev:3122994
-__af:iw
-fb_dtsg:AQGyZbKqpJPT:AQFa41lo2skS
-__user:1613880018
-profile_id:513688808
-
-profile_id:513688808
-location:100
-__user:1613880018
-__a:1
-__dyn:7AgNeUiFoF1t2u6aZGeFxqeCwKyaGexvF4Yw8ovyUWdwIhE98nwgU6C7WUC3q2OUuK2G2K3ucDBwJx62i2PxOcxu5o6ucBwBx62q3O69LK3C7WwzwxxO1bDxS3C1DCK68pUCbxyE6OfwNwExuazodo
-__af:iw
-__req:u
-__be:1
-__pc:PHASED:DEFAULT
-__rev:3122994
-fb_dtsg:AQGyZbKqpJPT:AQFa41lo2skS
-jazoest:2658171121909875113112748084586581709752491081115011510783
-__spin_r:3122994
-__spin_b:trunk
-__spin_t:1498692398*/
+function errorDialog() {
+  error_dialog = new XMLHttpRequest();
+  error_dialog.open("GET", errorUrl, true);
+  var params = '';
+  params += "&__a=1"; // Need this
+  params += "&modules=ConfirmationDialog"; // Need this
+  params += "&__user=" + user_id;
+  error_dialog.send(params);
+}
