597c04fcb63dfc606efbc466596e65227320a821
rak99 <rhenierkloppers124@gmail.com>
2019-01-09 23:51:05 +0200

    Touchscreen, resolution and element hiding
    Touchscreen and resolution bugs fixed, functionality to reset element hiding added

---
+++ divs.js
@@ -0,0 +1,132 @@
+var divs = {
+  "stackoverflow.com": [
+    {
+      name: "hot-network-questions",
+      type: "id",
+      hidden: true
+    }
+  ],
+  "dailymail.co.uk": [
+    {
+      name: "femail-item",
+      type: "class",
+      hidden: true
+    },
+    {
+      name: "sport-item",
+      type: "class",
+      hidden: true
+    },
+    {
+      name: "tvshowbiz item",
+      type: "class",
+      hidden: true
+    }
+  ],
+  "mailonline.com": [
+    {
+      name: "femail-item",
+      type: "class",
+      hidden: true
+    },
+    {
+      name: "sport-item",
+      type: "class",
+      hidden: true
+    },
+    {
+      name: "home item html_snippet_module",
+      type: "class",
+      hidden: true
+    },
+    {
+      name: "tvshowbiz item",
+      type: "class",
+      hidden: true
+    }
+  ],
+  "twitter.com": [
+    {
+      name: "module Trends trends",
+      type: "class",
+      hidden: true
+    },
+    {
+      name: "module wtf-module js-wtf-module roaming-module has-content",
+      type: "class",
+      hidden: true
+    }
+  ],
+  "buzzfeed.com": [
+    {
+      name: "mod-site-component-list-1",
+      type: "id",
+      hidden: true
+    },
+    {
+      name: "mod-site-component-list-2",
+      type: "id",
+      hidden: true
+    },
+    {
+      name: "now-buzzing",
+      type: "class",
+      hidden: true
+    },
+    {
+      name: "feedgrid",
+      type: "data-module",
+      hidden: true
+    }
+  ],
+  "theguardian.com": [
+    {
+      name: "most-viewed",
+      type: "id",
+      hidden: true
+    },
+    {
+      name: "more-on-this-story",
+      type: "id",
+      hidden: true
+    }
+  ],
+  "youtube.com": [
+    {
+      name: "related",
+      type: "id",
+      hidden: true
+    }
+  ],
+  // "linkedin.com": [
+  //   {
+  //     name: "relative ember-view",
+  //     type: "class",
+  //     hidden: true
+  //   }
+  // ],
+  "facebook.com": [
+    {
+      name: "pagelet_navigation",
+      type: "id",
+      hidden: true
+    },
+    {
+      name: "fallback_feed",
+      type: "id",
+      hidden: true
+    },
+    {
+      name: "rightCol",
+      type: "id",
+      hidden: true
+    }
+  ]
+};
+
+// urls = [
+//   "stackoverflow.com", "dailymail.co.uk", "mailonline.com", 
+//   "twitter.com", "twitter.com", "buzzfeed.com", "theguardian.com", 
+//   "youtube.com", "facebook.com"
+// ];
+
