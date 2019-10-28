b1d75bc7b7952ba0d9a41e5b08158188abc4ae58
Louis Barclay <louis.barclay@gmail.com>
2019-10-28 18:34:52 +0200

    fix(dev): basic env setup

--- extension/config.js
+++ extension/config.js
@@ -1,9 +1,19 @@
 // Release values in notes
 
 var config = {
-  debug: false, // false
-  offByDefault: true, // true
-  resetDivSettings: false, // false
-  fastTimer: false, // false
-  timelineTest: false // false
+  debug: false,
+  offByDefault: true,
+  resetDivSettings: false,
+  fastTimer: false,
+  timelineTest: false
+}
+
+// Dev config variables
+if (!chrome.runtime.getManifest().update_url) {
+  console.log("In dev env")
+  config.debug = true
+  config.offByDefault = true
+  config.resetDivSettings = false
+  config.fastTimer = false
+  config.timelineTest = false
 }
