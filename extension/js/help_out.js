525533d1b9fd47c3c7df37a4fbe9061b4fb7b52f
Louis Barclay <louis.barclay@gmail.com>
2019-11-11 14:47:50 +0200

    some inactive JS for help out page

---
+++ extension/js/help_out.js
@@ -0,0 +1,9 @@
+console.log("here and ready for duty")
+
+var url = "https://us11.api.mailchimp.com/3.0/lists/d3b4c5f9ac/members/"
+
+// Adding a new domain
+el("subscribe").addEventListener("keydown", function(event) {
+  if (event.key === "Enter") {
+  }
+})
