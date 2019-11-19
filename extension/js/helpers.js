e3d52c8f10868bb06758775431c2568c3c27495e
Louis Barclay <louis.barclay@gmail.com>
2019-11-19 12:12:57 +0200

    add Amplitude function to helpers

--- extension/js/helpers.js
+++ extension/js/helpers.js
@@ -9,6 +9,10 @@ function extractDomain(url) {
   return niceUrl.hostname + "/" + niceUrl.pathname.split("/")[1]
 }
 
+function getUrl(path) {
+  return chrome.extension.getURL(path)
+}
+
 ;(function(funcName, baseObj) {
   // The public function name defaults to window.docReady
   // but you can pass in your own object and own function name and those will be used
@@ -166,10 +170,6 @@ function popupCenter(url, title, w, h) {
   if (window.focus) {
     newWindow.focus()
   }
-}
-
-function getUrl(path) {
-  return chrome.extension.getURL(path)
 }
 
 function sendHTMLRequest(url, callback, errorFunction) {
@@ -492,6 +492,100 @@ function fbTokenReady(name, callback) {
     characterData: false
   })
 }
+
+// Amplitude
+;(function(e, t) {
+  var n = e.amplitude || { _q: [], _iq: {} }
+  var r = t.createElement("script")
+  r.type = "text/javascript"
+  r.integrity =
+    "sha384-d/yhnowERvm+7eCU79T/bYjOiMmq4F11ElWYLmt0ktvYEVgqLDazh4+gW9CKMpYW"
+  r.crossOrigin = "anonymous"
+  r.async = true
+  r.src = getUrl("js/vendor/amplitude-5.2.2-min.gz.js")
+  r.onload = function() {
+    if (!e.amplitude.runQueuedFunctions) {
+      console.log("[Amplitude] Error: could not load SDK")
+    }
+  }
+  var i = t.getElementsByTagName("script")[0]
+  i.parentNode.insertBefore(r, i)
+  function s(e, t) {
+    e.prototype[t] = function() {
+      this._q.push([t].concat(Array.prototype.slice.call(arguments, 0)))
+      return this
+    }
+  }
+  var o = function() {
+    this._q = []
+    return this
+  }
+  var a = ["add", "append", "clearAll", "prepend", "set", "setOnce", "unset"]
+  for (var u = 0; u < a.length; u++) {
+    s(o, a[u])
+  }
+  n.Identify = o
+  var c = function() {
+    this._q = []
+    return this
+  }
+  var l = [
+    "setProductId",
+    "setQuantity",
+    "setPrice",
+    "setRevenueType",
+    "setEventProperties"
+  ]
+  for (var p = 0; p < l.length; p++) {
+    s(c, l[p])
+  }
+  n.Revenue = c
+  var d = [
+    "init",
+    "logEvent",
+    "logRevenue",
+    "setUserId",
+    "setUserProperties",
+    "setOptOut",
+    "setVersionName",
+    "setDomain",
+    "setDeviceId",
+    "setGlobalUserProperties",
+    "identify",
+    "clearUserProperties",
+    "setGroup",
+    "logRevenueV2",
+    "regenerateDeviceId",
+    "groupIdentify",
+    "onInit",
+    "logEventWithTimestamp",
+    "logEventWithGroups",
+    "setSessionId",
+    "resetSessionId"
+  ]
+  function v(e) {
+    function t(t) {
+      e[t] = function() {
+        e._q.push([t].concat(Array.prototype.slice.call(arguments, 0)))
+      }
+    }
+    for (var n = 0; n < d.length; n++) {
+      t(d[n])
+    }
+  }
+  v(n)
+  n.getInstance = function(e) {
+    e = (!e || e.length === 0 ? "$default_instance" : e).toLowerCase()
+    if (!n._iq.hasOwnProperty(e)) {
+      n._iq[e] = { _q: [] }
+      v(n._iq[e])
+    }
+    return n._iq[e]
+  }
+  e.amplitude = n
+})(window, document)
+
+amplitude.getInstance().init("a5d19f66e1e6891cd22dcb350a04b08a")
 
 // Check if in domains setting
 function domainCheck(url, settings) {
