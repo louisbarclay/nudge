a955647f938920bd07d6a3718e99025797501327
Louis Barclay <louis.barclay@gmail.com>
2019-12-31 14:41:11 +0000

    dev tools: add blur option for img.js

--- img.js
+++ img.js
@@ -2,26 +2,27 @@
 
 // Use it by running 'node img.js' in terminal
 
-var sharp = require("sharp");
-var fs = require("fs");
+var sharp = require("sharp")
+var fs = require("fs")
 
-var dir = "extension/img/bg";
-const bgDir = fs.readdirSync(dir);
-console.log(bgDir);
+var dir = "extension/img/bg"
+const bgDir = fs.readdirSync(dir)
+console.log(bgDir)
 
-var bgImages = [];
+var bgImages = []
 
-bgDir.forEach(function (item) {
+bgDir.forEach(function(item) {
   if (item.includes(".jpg")) {
-    bgImages.push(item);
+    bgImages.push(item)
     var originalImage = sharp(fs.readFileSync(`${dir}/${item}`))
-      .resize(200)
+      // .resize(200)
+      .blur(20)
       // .greyscale() // Extra intensity of nudge here
       .toBuffer()
       .then(data =>
-        fs.writeFile(`${dir}/small/${item}`, data, "binary", function (err) { })
+        fs.writeFile(`${dir}/blur/${item}`, data, "binary", function(err) {})
       )
-      .catch(err => console.log(err));
+      .catch(err => console.log(err))
     // var newImage = sharp(fs.readFileSync(`${dir}/${item}`))
     //   .resize(1920)
     //   .toBuffer()
@@ -30,12 +31,12 @@ bgDir.forEach(function (item) {
     //   )
     //   .catch(err => console.log(err));
   }
-});
+})
 
 // Output a JS array of all bg files
 fs.writeFile(
   `extension/js/bgImages.js`,
   `var bgImages = ${JSON.stringify(bgImages)};`,
   "binary",
-  function (err) { }
-);
+  function(err) {}
+)
