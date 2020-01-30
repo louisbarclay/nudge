7c3788e4fcc6e3d4aa22424ff25210edd31eadcb
Louis Barclay <louis.barclay@gmail.com>
2020-01-30 17:57:03 +0200

    JS for unfollower settings page

---
+++ extension/js/unfollower.js
@@ -0,0 +1,14 @@
+var hiddenSections = el("js-dropdown-contents")
+var hiddenSectionsToggle = el("js-dropdown-button")
+
+let showHiddenSections = false
+hiddenSectionsToggle.onclick = function() {
+  toggleClass(hiddenSections, "display-none")
+  toggleClass(hiddenSectionsToggle, "expanded")
+
+  if (showHiddenSections) {
+    showHiddenSections = false
+  } else {
+    showHiddenSections = true
+  }
+}
