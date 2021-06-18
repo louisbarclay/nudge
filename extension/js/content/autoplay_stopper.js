b3d68b3a98834aa09cc579a81ee67eaaa56964d5
Louis Barclay <hi@louis.work>
2021-06-18 16:47:57 +0200

    fix autoplay_stopper

--- extension/js/content/autoplay_stopper.js
+++ extension/js/content/autoplay_stopper.js
@@ -1,63 +1,105 @@
 async function autoplayStopper() {
-  let observe = true
+  let observe = true;
 
   // Start observing the document
-  observeDoc()
+  observeDoc();
 
   // Only look for toggle for 60 seconds, after which give up and disconnect observer
   docReady(() => {
     // log("Begin time out")
     setTimeout(() => {
       // log("Time out: disconnect observer")
-      observe = false
-    }, 60000)
-  })
+      observe = false;
+    }, 60000);
+  });
 
   // Set toggle
   async function setToggle() {
     // There are two possible toggles
-    const toggleOriginal = el("toggle")
-    const toggleAlternative = el("improved-toggle")
-    // Check both of them
-    const toggleArray = [toggleOriginal, toggleAlternative]
-    toggleArray.forEach(async (toggle) => {
-      // Check that toggle exists
-      if (toggle) {
-        if (toggle.attributes.checked) {
-          // That particular toggle exists and is checked
-          // We change its value
+    const toggleOriginal = el("toggle");
+    const toggleAlternative = el("improved-toggle");
+    const toggleNew =
+      document.getElementsByClassName("ytp-button").length > 0 &&
+      Array.from(document.getElementsByClassName("ytp-button")).find(
+        (element) => {
+          let label = element.getAttribute("aria-label");
+          if (label && label.toLowerCase().includes("auto-play")) {
+            return true;
+          }
+        }
+      );
+
+    if (toggleNew) {
+      // Check that toggle children exist
+      if (
+        toggleNew.firstElementChild &&
+        toggleNew.firstElementChild.firstElementChild
+      ) {
+        // Read property to see if toggle checked
+        let toggleChecked =
+          toggleNew.firstElementChild.firstElementChild.getAttribute(
+            "aria-checked"
+          ) === "true";
+
+        if (toggleChecked) {
           eventLogSender("youtube_autoplay", {
             type: "off_auto",
             domain: "youtube.com",
-          })
-          toggle.click()
+          });
+          toggleNew.click();
           // log(`Clicked ${toggle.id}`)
-          observe = false
         } else {
           eventLogSender("youtube_autoplay", {
             type: "already_off",
             domain: "youtube.com",
-          })
+          });
           // The toggle exists and is not checked
           // We can stop observing
-          observe = false
-        }
-        // Also attach an onclick to toggle
-        toggle.onclick = function () {
-          if (toggle.attributes.checked) {
-            eventLogSender("youtube_autoplay", {
-              type: "on_manual",
-              domain: "youtube.com",
-            })
-          } else {
-            eventLogSender("youtube_autoplay", {
-              type: "off_manual",
-              domain: "youtube.com",
-            })
-          }
+          observe = false;
         }
       }
-    })
+    } else {
+      // Check both of them
+      const toggleArray = [toggleOriginal, toggleAlternative, toggleNew];
+      toggleArray.forEach(async (toggle) => {
+        // Check that toggle exists
+        if (toggle) {
+          if (toggle.attributes.checked) {
+            // That particular toggle exists and is checked
+            // We change its value
+            eventLogSender("youtube_autoplay", {
+              type: "off_auto",
+              domain: "youtube.com",
+            });
+            toggle.click();
+            // log(`Clicked ${toggle.id}`)
+            observe = false;
+          } else {
+            eventLogSender("youtube_autoplay", {
+              type: "already_off",
+              domain: "youtube.com",
+            });
+            // The toggle exists and is not checked
+            // We can stop observing
+            observe = false;
+          }
+          // Also attach an onclick to toggle
+          toggle.onclick = function () {
+            if (toggle.attributes.checked) {
+              eventLogSender("youtube_autoplay", {
+                type: "on_manual",
+                domain: "youtube.com",
+              });
+            } else {
+              eventLogSender("youtube_autoplay", {
+                type: "off_manual",
+                domain: "youtube.com",
+              });
+            }
+          };
+        }
+      });
+    }
   }
 
   // Observe doc
@@ -66,19 +108,19 @@ async function autoplayStopper() {
       // You don't use any mutation info
       // It's just a convenient trigger
       () => {
-        setToggle()
+        setToggle();
         if (!observe) {
           // log("Disconnected observer")
-          observer.disconnect()
+          observer.disconnect();
         }
       }
-    )
+    );
 
     // Observe entire document and childList to capture all mutations
     observer.observe(document, {
       childList: true,
       subtree: true,
       characterData: false,
-    })
+    });
   }
 }
