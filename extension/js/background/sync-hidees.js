1ef46e3dfbca2fec0482e862f03ec9f063aee16c
Thor Galle <thorgalle@gmail.com>
2021-10-02 23:04:41 +0300

    Replace deprecated GetSheetDone with pure Google Sheet API

--- extension/js/background/sync-hidees.js
+++ extension/js/background/sync-hidees.js
@@ -1,42 +1,47 @@
+if (!NUDGE_ENV) {
+  log("warning: Google Sheet v4 API values are missing");
+}
+
+const url = `https://sheets.googleapis.com/v4/spreadsheets/${NUDGE_ENV.HIDEES_GOOGLE_SHEET}/values/Hidden Sections?key=${NUDGE_ENV.GOOGLE_API_KEY}`
+
 async function checkHidees() {
-  return new Promise((resolve) => {
+  return new Promise(async (resolve) => {
     try {
-      raw("1Y-xQb1qRgnicD_0M-8REXKbSPi4lnsQW7lqUqh52fiw").then((sheet) => {
-        log("Connecting to Google Sheet")
-        let hidees = []
-        // Get row headers
-        const keys = sheet.data[0]
-        // Exclude row headers
-        sheet.data.forEach((row, index) => {
-          if (index > 0) {
-            let hidee = {}
-            row.forEach((columnValue, index) => {
-              // Clean column values
-              if (columnValue) {
-                if (keys[index].includes("style.")) {
-                  if (!hidee.style) {
-                    hidee.style = {}
-                  }
-                  hidee.style[keys[index].replace("style.", "")] = columnValue
-                } else {
-                  columnValue = cleanValue(columnValue)
-                  hidee[keys[index]] = columnValue
+      const rows = await fetch(url).then(r => r.json()).then(obj => obj.values);
+      log("Connecting to Google Sheet")
+      let hidees = []
+      // Get row headers
+      const keys = rows[0]
+      // Exclude row headers
+      rows.forEach((row, index) => {
+        if (index > 0) {
+          let hidee = {}
+          row.forEach((columnValue, index) => {
+            // Clean column values
+            if (columnValue) {
+              if (keys[index].includes("style.")) {
+                if (!hidee.style) {
+                  hidee.style = {}
                 }
+                hidee.style[keys[index].replace("style.", "")] = columnValue
+              } else {
+                columnValue = cleanValue(columnValue)
+                hidee[keys[index]] = columnValue
               }
-            })
-            // Make a hidee style object if there is none
-            if (!hidee.style) {
-              hidee.style = {}
             }
-            if (hidee.import) {
-              delete hidee.import
-              hidees.push(hidee)
-            }
+          })
+          // Make a hidee style object if there is none
+          if (!hidee.style) {
+            hidee.style = {}
           }
-        })
-        log("Loaded hidees")
-        resolve(hidees)
+          if (hidee.import) {
+            delete hidee.import
+            hidees.push(hidee)
+          }
+        }
       })
+      log("Loaded hidees")
+      resolve(hidees)
     } catch (e) {
       resolve(false)
     }
---
+++ sync-lib/export-environment.js
@@ -0,0 +1,21 @@
+// run from root
+// export the dotenv config to js/background/env.js,
+// for use in web
+require("dotenv").config();
+const fs = require("fs")
+
+const envLocation = 'extension/js/background/env.js'
+const keys = ['GOOGLE_API_KEY', 'HIDEES_GOOGLE_SHEET']
+const envObject = keys.reduce((obj, key) => ({
+    ...obj,
+    [key]: process.env[key]
+}), {});
+
+fs.writeFile(
+    envLocation,
+    `const NUDGE_ENV = ${JSON.stringify(envObject)}`,
+    function (err) {
+        if (err) return console.log(err)
+        console.log(`Saved new hidees from Google Sheet`)
+    }
+)
