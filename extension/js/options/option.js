13106791548469b97fda510d9ea0489ec7916110
Louis Barclay <hi@louis.work>
2021-11-01 12:24:20 +0000

    fix/use different arrow symbol

--- extension/js/options/option.js
+++ extension/js/options/option.js
@@ -1,10 +1,10 @@
-var hash = window.location.hash.substr(1)
+var hash = window.location.hash.substr(1);
 
-eventLogSender("optionsPage", { page: window.location.pathname.split("/")[3] })
+eventLogSender("optionsPage", { page: window.location.pathname.split("/")[3] });
 
 // Hide onboarding if coming from options home
 if (hash === "options") {
-  toggleClass(el("js-onboarding"), "display-none")
+  toggleClass(el("js-onboarding"), "display-none");
 }
 
 var optionsIndex = [
@@ -38,159 +38,159 @@ var optionsIndex = [
     page: "done.html",
     name: "done!",
   },
-]
+];
 
 for (var i = 0; i < optionsIndex.length; i++) {
   if (window.location.href.includes(optionsIndex[i].page)) {
     if (hash === "options") {
-      el("js-back").innerHTML = `🠐 Options`
+      el("js-back").innerHTML = `← Options`;
       el("js-back").onclick = function () {
-        window.location = `./options_home.html`
-      }
+        window.location = `./options_home.html`;
+      };
     } else if (hash === "unfollower") {
-      el("js-back").innerHTML = `🠐 Welcome to Nudge`
+      el("js-back").innerHTML = `← Welcome to Nudge`;
       el("js-back").onclick = function () {
-        window.location = `./start.html`
-      }
-      el("js-onboarding-skip").innerHTML = "No, thanks"
-      el("js-next").innerHTML = "Check out Nudge's other features"
+        window.location = `./start.html`;
+      };
+      el("js-onboarding-skip").innerHTML = "No, thanks";
+      el("js-next").innerHTML = "Check out Nudge's other features";
       el("js-next").onclick = function () {
-        window.location = `./options_home.html`
-      }
+        window.location = `./options_home.html`;
+      };
     } else if (
       window.location.href.includes("options_home.html") &&
       hash != "onboarding"
     ) {
-      el("js-back").innerHTML = ``
+      el("js-back").innerHTML = ``;
     } else {
       try {
         // Catch possible error on Change Site page
         if (optionsIndex[i - 1]) {
           // Set up back link
-          el("js-back").innerHTML = `🠐 ${optionsIndex[i - 1].name}`
+          el("js-back").innerHTML = `← ${optionsIndex[i - 1].name}`;
           el("js-back").onclick = function () {
-            window.location = `./${optionsIndex[i - 1].page}`
-          }
+            window.location = `./${optionsIndex[i - 1].page}`;
+          };
         }
       } catch (e) {
-        log(e)
+        log(e);
       }
       try {
         // Set up next link
         if (window.location.href.includes("scheduler.html")) {
           el(
             "js-next"
-          ).innerHTML = `Next: choose which of Nudge's features to enable`
+          ).innerHTML = `Next: choose which of Nudge's features to enable`;
         } else {
-          el("js-next").innerHTML = `Next: ${optionsIndex[i + 1].name}`
+          el("js-next").innerHTML = `Next: ${optionsIndex[i + 1].name}`;
         }
         el("js-next").onclick = function () {
-          window.location = `./${optionsIndex[i + 1].page}`
-        }
+          window.location = `./${optionsIndex[i + 1].page}`;
+        };
       } catch (e) {}
-      break
+      break;
     }
   }
 }
 
 function handleToggle(element, callback, override, onFirstClick) {
-  var left = element.childNodes[0]
-  var newVal = left.className.includes("on")
-  var right = element.childNodes[1]
+  var left = element.childNodes[0];
+  var newVal = left.className.includes("on");
+  var right = element.childNodes[1];
   if (override) {
     if (newVal) {
-      toggleClass(left, "on")
-      toggleClass(right, "on")
+      toggleClass(left, "on");
+      toggleClass(right, "on");
     }
   } else {
-    toggleClass(left, "on")
-    toggleClass(right, "on")
+    toggleClass(left, "on");
+    toggleClass(right, "on");
     if (newVal && onFirstClick) {
       if (el("js-onboarding-enable")) {
-        el("js-onboarding-enable").parentNode.style.display = "none"
-        el("js-next").parentNode.style.display = "flex"
+        el("js-onboarding-enable").parentNode.style.display = "none";
+        el("js-next").parentNode.style.display = "flex";
       }
     }
   }
   if (callback) {
-    callback(newVal)
+    callback(newVal);
   }
 }
 
 if (el("js-onboarding-enable")) {
   el("js-onboarding-enable").onclick = function () {
-    handleOnboardingClick(this, true)
-  }
+    handleOnboardingClick(this, true);
+  };
 }
 
 if (el("js-onboarding-skip")) {
   el("js-onboarding-skip").onclick = function () {
-    handleOnboardingClick(this, false)
-  }
+    handleOnboardingClick(this, false);
+  };
 }
 
-;(async () => {
-  var settings = await loadSettingsRequest()
+(async () => {
+  var settings = await loadSettingsRequest();
   // Find any main toggles and set correct value + handle click
   Array.from(document.getElementsByClassName("toggle")).forEach(function (
     element
   ) {
     // This will only work if the parentNode of the element has an id that's a valid setting string
-    var setting = element.parentNode.id
-    var left = element.childNodes[0]
-    var currentVal = !left.className.includes("on")
+    var setting = element.parentNode.id;
+    var left = element.childNodes[0];
+    var currentVal = !left.className.includes("on");
 
     // Only change the onboarding thing on the very first click
-    let onFirstClick = true
+    let onFirstClick = true;
     element.addEventListener("click", function (e) {
       // e.stopPropagation()
       // e.stopImmediatePropagation()
-      e.preventDefault()
+      e.preventDefault();
       handleToggle(
         element,
         function handleSettingChange(value) {
-          changeSettingRequest(value, setting)
+          changeSettingRequest(value, setting);
         },
         null,
         onFirstClick
-      )
-      onFirstClick = false
-    })
+      );
+      onFirstClick = false;
+    });
 
     if (currentVal !== settings[setting]) {
-      handleToggle(element, null)
+      handleToggle(element, null);
     }
-  })
+  });
   // Find any subSetting checkboxes set correct value + handle click
   Array.from(document.getElementsByClassName("checkbox-setting")).forEach(
     function (element) {
-      var subSetting = element.childNodes[0].id
-      var checkbox = element.childNodes[0]
+      var subSetting = element.childNodes[0].id;
+      var checkbox = element.childNodes[0];
       // Set current setting
       if (subSetting in settings && settings[subSetting]) {
-        checkbox.checked = true
+        checkbox.checked = true;
       }
       // Handle click
       checkbox.onclick = function () {
-        changeSettingRequest(checkbox.checked, subSetting)
-      }
+        changeSettingRequest(checkbox.checked, subSetting);
+      };
     }
-  )
+  );
   // Plug in Nudge ID if we have it
-  var nudgeId = el("js-nudge-id")
+  var nudgeId = el("js-nudge-id");
   if (nudgeId) {
-    nudgeId.innerHTML = settings.userId
+    nudgeId.innerHTML = settings.userId;
   }
-})()
+})();
 
 function handleOnboardingClick(element, changeSetting) {
-  var parent = element.parentNode
-  var setting = parent.parentNode.parentNode.id
+  var parent = element.parentNode;
+  var setting = parent.parentNode.parentNode.id;
   if (changeSetting) {
-    var toggle = document.getElementsByClassName("toggle")[0]
-    handleToggle(toggle, null, true)
-    changeSettingRequest(true, setting)
+    var toggle = document.getElementsByClassName("toggle")[0];
+    handleToggle(toggle, null, true);
+    changeSettingRequest(true, setting);
   }
-  parent.style.display = "none"
-  el("js-next").parentNode.style.display = "flex"
+  parent.style.display = "none";
+  el("js-next").parentNode.style.display = "flex";
 }
